1190afc18e3bd24233936fd7ee5a3e47dd5b4bc8
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-05-26 09:40:41 -0600

    edit

--- BarHopper.client/app/env.js
+++ BarHopper.client/app/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'dev-zmsf17trpyd7nhu5.us.auth0.com'
-export const audience = 'http://sandbox.com'
-export const clientId = '7GPfU0YWrSDqdocvzTEar4vjEV3o4sjh'
+export const domain = 'dev-vle07jgfwkf0qxzw.us.auth0.com'
+export const audience = 'http://fishbowl.com'
+export const clientId = 'NBELLFVM5dnrCzL54uRsTSVLfoQQymek'
