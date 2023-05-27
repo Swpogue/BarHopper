6b7258f45de408bf6c3716de1712d5fdc27c7a9f
Nathan Moehlmann <nathan.moehlmann@gmail.com>
2023-05-26 19:50:53 -0600

    post comments working

--- BarHopper.client/app/services/CommentsService.js
+++ BarHopper.client/app/services/CommentsService.js
@@ -3,19 +3,33 @@ import { api } from "./AxiosService.js"
 import { Comment } from "../models/Comment.js"
 
 class CommentsService {
+  async createComment(formData) {
+
+    let barId = AppState.activeBar?.id
+    let description = formData.description
+    let hopperId = AppState.account?.id
+
+    let post = { barId, description, hopperId }
+    console.log(post);
+    const res = await api.post('api/comments', post)
+    console.log(res.data);
+
+  }
   getCommentsForActiveBar() {
     const bar = AppState.activeBar
   }
 
+  // TODO save the active bar to local storage sometime
+
 
   async getCommentsByBarId() {
     let id = AppState.activeBar?.id
-    console.log(id);
+    // console.log(id);
     const res = await api.get(`api/comments/bar/${id}`)
-    console.log(res.data);
+    // console.log(res.data);
     AppState.comments = res.data.map(c => new Comment(c))
     AppState.emit('comments')
-    console.log(AppState.comments);
+    // console.log(AppState.comments);
   }
 }
 
